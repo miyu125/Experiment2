97403c04ae883ba86aba8ef9fcfe8a946e065a91
pipipi_miyu <suzuki.miyu.17b@shizuoka.ac.jp>
2019-10-11 15:21:37 +0900

    second commit

--- src/lang/c/parse/Expression.java
+++ src/lang/c/parse/Expression.java
@@ -21,11 +21,19 @@ public class Expression extends CParseRule {
 		term.parse(pcx);
 		CTokenizer ct = pcx.getTokenizer();
 		CToken tk = ct.getCurrentToken(pcx);
-		while (ExpressionAdd.isFirst(tk)) {
-			list = new ExpressionAdd(pcx, term);
-			list.parse(pcx);
-			term = list;
-			tk = ct.getCurrentToken(pcx);
+		while(ExpressionAdd.isFirst(tk)||ExpressionSub.isFirst(tk)) {		// "+"だけでなく"-"も認識するようにExpressionSub.isFirst()を追加
+			while (ExpressionAdd.isFirst(tk)) {
+				list = new ExpressionAdd(pcx, term);
+				list.parse(pcx);
+				term = list;
+				tk = ct.getCurrentToken(pcx);
+			}
+			while (ExpressionSub.isFirst(tk)) {
+				list = new ExpressionSub(pcx, term);
+				list.parse(pcx);
+				term = list;
+				tk = ct.getCurrentToken(pcx);
+			}	
 		}
 		expression = term;
 	}
@@ -100,6 +108,65 @@ class ExpressionAdd extends CParseRule {
 			o.println("\tMOV\t-(R6), R1\t; ExpressionAdd:");
 			o.println("\tADD\tR1, R0\t; ExpressionAdd:");
 			o.println("\tMOV\tR0, (R6)+\t; ExpressionAdd:");
+		}
+	}
+}
+
+
+class ExpressionSub extends CParseRule {
+	// expressionSub ::= '-' term
+	private CToken op;
+	private CParseRule left, right;
+	public ExpressionSub(CParseContext pcx, CParseRule left) {
+		this.left = left;
+	}
+	public static boolean isFirst(CToken tk) {
+		return tk.getType() == CToken.TK_MINUS;
+	}
+	public void parse(CParseContext pcx) throws FatalErrorException {
+		// ここにやってくるときは、必ずisFirst()が満たされている
+		CTokenizer ct = pcx.getTokenizer();
+		op = ct.getCurrentToken(pcx);
+		// +の次の字句を読む
+		CToken tk = ct.getNextToken(pcx);
+		if (Term.isFirst(tk)) {
+			right = new Term(pcx);
+			right.parse(pcx);
+		} else {
+			pcx.fatalError(tk.toExplainString() + "+の後ろはtermです");
+		}
+	}
+
+	public void semanticCheck(CParseContext pcx) throws FatalErrorException {
+		// 引き算の型計算規則
+		final int s[][] = {
+		//		T_err			T_int
+			{	CType.T_err,	CType.T_err },	// T_err
+			{	CType.T_err,	CType.T_int },	// T_int
+		};
+		if (left != null && right != null) {
+			left.semanticCheck(pcx);
+			right.semanticCheck(pcx);
+			int lt = left.getCType().getType();		// -の左辺の型
+			int rt = right.getCType().getType();	// -の右辺の型
+			int nt = s[lt][rt];						// 規則による型計算
+			if (nt == CType.T_err) {
+				pcx.fatalError(op.toExplainString() + "左辺の型[" + left.getCType().toString() + "]と右辺の型[" + right.getCType().toString() + "]は引けません");
+			}
+			this.setCType(CType.getCType(nt));
+			this.setConstant(left.isConstant() && right.isConstant());	// +の左右両方が定数のときだけ定数
+		}
+	}
+
+	public void codeGen(CParseContext pcx) throws FatalErrorException {
+		PrintStream o = pcx.getIOContext().getOutStream();
+		if (left != null && right != null) {
+			left.codeGen(pcx);		// 左部分木のコード生成を頼む
+			right.codeGen(pcx);		// 右部分木のコード生成を頼む
+			o.println("\tMOV\t-(R6), R0\t; ExpressionSub: ２数を取り出して、引き、積む<" + op.toString() + ">");					// SUB　FFFFF　TTTTT　のとき
+			o.println("\tMOV\t-(R6), R1\t; ExpressionSub:");															// T－F　→　T　となる
+			o.println("\tSUB\tR0, R1\t; ExpressionAdd:");																// したがって　レジスタは　後入れ　－　先入れ
+			o.println("\tMOV\tR1, (R6)+\t; ExpressionSub:");
 		}
 	}
 }
